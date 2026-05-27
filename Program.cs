ee34bdc5ce9048b93a21f7dcff8b3d38dc6e9c0f
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-05-27 13:48:47 +0300

    main => fix

--- Program.cs
+++ Program.cs
@@ -7,28 +7,28 @@ using DotNetEnv;
 
 Env.Load();
 
-var builder = WebApplication.CreateBuilder(args);
+var builder =
+    WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 EnvVars.CheckEnvVars();
 
+await Redis.Connect(app);
+
 if (app.Environment.IsDevelopment())
+{
     app.MapOpenApi();
+}
 
 app.UseHttpsRedirection();
 
-RateLimitExt.UseRateLimit(app);
+app.UseRateLimit();
 
 MainMiddleware.UseMainMiddleware(app);
 
 Router.MapApi(app);
 
-await Redis.Connect(app);
-
 app.Run();
-
